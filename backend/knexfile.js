cbe60f928f9974c93d15562f6d9bbfa6973625bc
Aravindh-2002-17 <aravindh7675@gmail.com>
2026-01-20 17:19:07 +0530

    fix mysql ssl and port

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,17 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
+      port: 3306, // IMPORTANT
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false // REQUIRED for Aiven
+      }
     },
     migrations: {
-      directory: './src/db/migrations',
-    },
-  },
+      directory: "./src/db/migrations"
+    }
+  }
 };
